828d13bdca42941e6f10f72693a528e221c84d40
Elshan Magsudov <elio@jyanik.com>
2022-11-10 00:14:10 -0500

    refactored by implementing module.exports

--- assertEqual.js
+++ assertEqual.js
@@ -2,8 +2,5 @@ const assertEqual = function(actual, expected) {
   console.log(actual === expected ? `✅✅✅Assertion Passed: ${actual} === ${expected}` : `🙈🙈🙈Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse", "Lighthouse");
-assertEqual(1, -98);
+module.exports = assertEqual;
+
--- head.js
+++ head.js
@@ -1,13 +1,8 @@
-const assertEqual = function(actual, expected) {
-  console.log(actual === expected ? `✅✅✅Assertion Passed: ${actual} === ${expected}` : `🙈🙈🙈Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function (arrayElement) {
   return arrayElement[0];
-}
+};
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head([-77, "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+
