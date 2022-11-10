c748781353f51974e616f477bce3d0048eeada0f
Elshan Magsudov <elio@jyanik.com>
2022-11-10 00:49:32 -0500

    refactored by implementing module.exports

--- middle.js
+++ middle.js
@@ -1,17 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length!==arr2.length) {
-    return false;
-  }
-  for (i=0; i<arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } return true  
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  console.log(eqArrays(arr1, arr2) ? `✅✅✅Assertion Passed: ${arr1} === ${arr2}` : `🙈🙈🙈Assertion Failed: ${arr1} !== ${arr2}`)
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const middle = function (source) {
@@ -26,24 +14,7 @@ const middle = function (source) {
   return middleArr
 }
 
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
-assertArraysEqual(middle([1]), [1]);
-assertArraysEqual(middle([1, 2]), ["2"]);
-
-assertArraysEqual(middle([1, 2, 3]), ["ap"]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), ['HOME', "3"]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), ["2", 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, "4"]);
+module.exports = middle;
 
 
 
