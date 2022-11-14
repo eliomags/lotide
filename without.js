19a30ec68e71778002b8b8ddf887b348aac17d66
Elshan Magsudov <elio@jyanik.com>
2022-11-14 12:08:38 -0500

    changed from looping both arrays to .includes method to avoid repetitive addition of item to the new array

--- without.js
+++ without.js
@@ -1,32 +1,16 @@
+const eqArrays = require("./eqArrays");
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length!==arr2.length) {
-    return false;
-  }
-  for (i=0; i<arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } return true  
-}
+const assertArraysEqual = require("./assertArraysEqual");
 
-const assertArraysEqual = function (arr1, arr2) {
-  console.log(eqArrays(arr1, arr2) ? `✅✅✅Assertion Passed: ${arr1} === ${arr2}` : `🙈🙈🙈Assertion Failed: ${arr1} !== ${arr2}`)
-}
-
-
-const without = function(source, itemsToRemove) {
+const without = function (source, itemsToRemove) {
   let newArr = [];
-  for (let i = 0 ; i < source.length; i++) {
-    for (let y=0; y<itemsToRemove.length; y++)
-   if (source[i] !== itemsToRemove[y]) {
-    newArr.push(source[i]);
-   }
-  } return newArr
-}
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) newArr.push(source[i]);
+  }
+  return newArr;
+};
 
-
-const words = ["hello", "world", "lighthouse"]; 
-without(words, ["lighthouse"]); // no need to capture return value for this test case
+const words = [1, 2, 3];
+console.log(without(words, [1, 2])); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
